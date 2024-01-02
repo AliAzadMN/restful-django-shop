d779e3cc873966c78a28b0770cf8b756220e6fc1
mr azad <ali.azadmanjiri2000@gmail.com>
2024-01-02 19:17:45 +0330

    feat: Add custom UserManager

--- core/models.py
+++ core/models.py
@@ -1,3 +1,27 @@
-from django.db import models
+from django.contrib.auth.models import BaseUserManager
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        """
+        Creates and saves a User with the given email & password.
+        """
+        if not email:
+            raise ValueError("Users must have an email address")
+        user = self.model(
+            email=self.normalize_email(email),
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        """
+        Creates and saves a superuser with the given email & password.
+        """
+        extra_fields.setdefault("is_admin", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        return self.create_user(email, password, **extra_fields)
+    
