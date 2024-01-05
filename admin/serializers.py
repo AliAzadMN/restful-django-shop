fcd23d629d098c2f609d9921faa7c86daeed3817
mr azad <ali.azadmanjiri2000@gmail.com>
2024-01-05 22:14:19 +0330

    feat: Add GroupCreateUpdateSerializer

--- admin/serializers.py
+++ admin/serializers.py
@@ -22,3 +22,42 @@ class GroupPermissionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Permission
         fields = ('id', )
+
+
+class GroupCreateUpdateSerializer(serializers.ModelSerializer):
+    permissions = GroupPermissionSerializer(many=True, required=False)
+
+    class Meta:
+        model = Group
+        fields = ('name', 'permissions', )
+
+    def create(self, validated_data):
+        permissions_data = validated_data.pop('permissions', [])
+
+        group = Group(**validated_data)
+        group.save()
+
+        permissions = [
+            Permission.objects.get(
+                id=permission['id'],
+            ) for permission in permissions_data
+        ]
+        group.permissions.set(permissions)
+        return group
+
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.save()
+
+        permissions_data = validated_data.get('permissions')
+
+        if permissions_data != None:
+            permissions = [
+                Permission.objects.get(
+                    id=permission['id'],
+                ) for permission in permissions_data
+        ]
+            instance.permissions.set(permissions)
+        
+        return instance
