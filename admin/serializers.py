d4348c0dc8006b5fa0442c4978a69c8a9f91a1d1
mr azad <ali.azadmanjiri2000@gmail.com>
2024-01-05 21:46:46 +0330

    feat: Add GroupListRetrieveSerializer

--- admin/serializers.py
+++ admin/serializers.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import Permission
+from django.contrib.auth.models import Group, Permission
 from rest_framework import serializers
 
 
@@ -6,4 +6,11 @@ class PermissionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Permission
         fields = ('id', 'name', 'codename', )
-        
+
+
+class GroupListRetrieveSerializer(serializers.ModelSerializer):
+    permissions = PermissionSerializer(many=True)
+
+    class Meta:
+        model = Group
+        fields = ('id', 'name', 'permissions', )     
