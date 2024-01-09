d67bdc3ee31a7fcfc0bdfe1269f603d80ef37bcf
mr azad <ali.azadmanjiri2000@gmail.com>
2024-01-09 22:53:53 +0330

    fix: Solve the problem of loading CSS & JS files with Return 'django.contrib.staticfiles' to INSTALLED_APPS

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.staticfiles',
 
     # local
     'core.apps.CoreConfig',
