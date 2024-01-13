5078e1ca482a40ed6d7e030ef0cc3aa0e6f21cce
mr azad <ali.azadmanjiri2000@gmail.com>
2024-01-13 20:59:57 +0330

    feat: Setup constants messages

---
+++ core/constants.py
@@ -0,0 +1,6 @@
+from django.utils.translation import gettext_lazy as _
+
+
+class Messages:
+    PASSWORD_MISMATCH_ERROR = _("The two password fields didn't match.")
+    CANNOT_CREATE_USER_ERROR = _("Unable to create account.")
