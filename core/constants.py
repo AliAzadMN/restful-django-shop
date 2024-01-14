b21d2581a52bc00dfdde1b10f0f1891119d0d981
mr azad <ali.azadmanjiri2000@gmail.com>
2024-01-14 21:25:02 +0330

    feat: Add INVALID_PASSWORD_ERROR to constants messages

--- core/constants.py
+++ core/constants.py
@@ -4,3 +4,4 @@ from django.utils.translation import gettext_lazy as _
 class Messages:
     PASSWORD_MISMATCH_ERROR = _("The two password fields didn't match.")
     CANNOT_CREATE_USER_ERROR = _("Unable to create account.")
+    INVALID_PASSWORD_ERROR = _("Invalid password.")
