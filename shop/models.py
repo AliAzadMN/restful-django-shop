f1be879b1c4289015fcbb9433d9952124d307ecc
mr azad <ali.azadmanjiri2000@gmail.com>
2024-01-23 09:17:54 +0330

    feat: Add Category model

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=500, blank=True)
+
+    def __str__(self):
+        return self.title
+    
