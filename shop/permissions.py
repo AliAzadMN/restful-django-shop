dccf53ffcc90af3d64d5466e513ca85c20d49c70
mr azad <ali.azadmanjiri2000@gmail.com>
2024-01-29 23:35:51 +0330

    feat: Add IsOwnerObject permission

--- shop/permissions.py
+++ shop/permissions.py
@@ -4,4 +4,10 @@ from rest_framework import permissions
 class IsInGroup(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.user.groups.filter(name=view.required_group).exists()
-    
+
+
+class IsOwnerObject(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return bool(
+            request.user and obj.user == request.user
+        )
