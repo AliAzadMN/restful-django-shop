ee2e5cf6020f87f912b7c8d55d11220e33cc37fe
mr azad <ali.azadmanjiri2000@gmail.com>
2024-01-30 23:47:03 +0330

    feat: Add full_name property to User model

--- core/models.py
+++ core/models.py
@@ -55,3 +55,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     def __str__(self):
         return self.email
     
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+        
